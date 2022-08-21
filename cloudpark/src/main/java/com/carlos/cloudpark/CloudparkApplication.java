392d6d8ccd5c3cb96a221801aa541eaee1ab45ec
carlos4565mana <carloscal61@gmail.com>
2022-08-21 16:41:15 -0300

    iniciando o projeto

---
+++ cloudpark/src/main/java/com/carlos/cloudpark/CloudparkApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.cloudpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudparkApplication.class, args);
+	}
+
+}
---
+++ cloudpark/src/test/java/com/carlos/cloudpark/CloudparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.cloudpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
